11e24f1520a1fd03deace192c952e66c3044e92b
kusalpranama <kusalpranama2003@gmail.com>
2024-11-10 20:59:02 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./pages/header/header.component";
+import { GamesComponent } from "./pages/games/games.component";
+import { TempComponent } from "./pages/temp/temp.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, GamesComponent, TempComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
